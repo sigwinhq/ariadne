8edc93ec0a036c87dab54627517ad6d0f2dc739b
Dalibor Karlović <dalibor.karlovic@sigwin.hr>
2023-02-18 17:46:20 +0100

    Initial Gitlab communication

--- src/Bridge/Symfony/Command/SyncCommand.php
+++ src/Bridge/Symfony/Command/SyncCommand.php
@@ -13,16 +13,32 @@ declare(strict_types=1);
 
 namespace Sigwin\Ariadne\Bridge\Symfony\Command;
 
+use Gitlab\Client;
 use Symfony\Component\Console\Attribute\AsCommand;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Console\Style\SymfonyStyle;
 
-#[AsCommand(name: 'ariadne:sync')]
+#[AsCommand(name: 'ariadne:sync', aliases: ['sync'])]
 final class SyncCommand extends Command
 {
+    public function __construct(private Client $client)
+    {
+        parent::__construct();
+    }
+
     public function execute(InputInterface $input, OutputInterface $output): int
     {
+        $style = new SymfonyStyle($input, $output);
+        $style->title('Sigwin Ariadne');
+
+        $version = $this->client->version()->show();
+        $style->info(sprintf('This is Gitlab %1$s', $version['version']));
+
+        $me = $this->client->users()->me();
+        $style->info(sprintf('You are %1$s, %2$s', $me['username'], $me['web_url']));
+
         return 0;
     }
 }
