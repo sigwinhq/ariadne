795d8214a570b384d68cf07c6c6269befe73bc63
Dalibor Karlović <dalibor.karlovic@sigwin.hr>
2023-02-18 17:13:45 +0100

    Initial

---
+++ src/Bridge/Symfony/Kernel.php
@@ -0,0 +1,21 @@
+<?php
+
+declare(strict_types=1);
+
+/*
+ * This file is part of the ariadne project.
+ *
+ * (c) sigwin.hr
+ *
+ * This source file is subject to the MIT license that is bundled
+ * with this source code in the file LICENSE.
+ */
+
+namespace Sigwin\Ariadne\Bridge\Symfony;
+
+use Symfony\Bundle\FrameworkBundle\Kernel\MicroKernelTrait;
+
+final class Kernel extends \Symfony\Component\HttpKernel\Kernel
+{
+    use MicroKernelTrait;
+}
