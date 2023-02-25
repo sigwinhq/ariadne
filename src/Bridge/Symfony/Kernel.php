4613bce6a5ca4760a20a3cee2471bcd779d391cb
Dalibor Karlović <dalibor.karlovic@sigwin.hr>
2023-02-25 18:18:04 +0100

    feat: PHAR (#31)

--- src/Bridge/Symfony/Kernel.php
+++ src/Bridge/Symfony/Kernel.php
@@ -18,4 +18,9 @@ use Symfony\Bundle\FrameworkBundle\Kernel\MicroKernelTrait;
 final class Kernel extends \Symfony\Component\HttpKernel\Kernel
 {
     use MicroKernelTrait;
+
+    public function getProjectDir(): string
+    {
+        return __DIR__.'/../../..';
+    }
 }
