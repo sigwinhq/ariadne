f162528a2041a4456cef46cf055223dcc41ecc37
Dalibor Karlović <dalibor.karlovic@sigwin.hr>
2023-02-28 12:52:40 +0100

    tests: initial GH profile tests, prepare for additional tests

--- tests/Bridge/Github/GithubProfileTest.php
+++ tests/Bridge/Github/GithubProfileTest.php
@@ -34,49 +34,73 @@ use Sigwin\Ariadne\ProfileTemplateFactory;
  */
 final class GithubProfileTest extends TestCase
 {
-    public function testCanFetchApiUser(): void
+    /**
+     * @dataProvider getUrls
+     */
+    public function testCanFetchApiUser(?string $baseUrl): void
     {
-        $httpClient = $this->getMockBuilder(ClientInterface::class)->getMock();
-        $httpClient
-            ->expects(static::once())
-            ->method('sendRequest')
-            ->willReturnCallback(static function (RequestInterface $request): Response {
-                self::assertSame('GET', $request->getMethod());
-                self::assertSame('https://api.github.com/user', $request->getUri()->__toString());
-                self::assertSame('token ABC', $request->getHeaderLine('Authorization'));
+        $httpClient = $this->mockHttpClient([
+            $this->generateUrl($baseUrl, '/user') => '{"login": "ariadne"}',
+        ]);
+        $factory = $this->mockTemplateFactory();
+        $config = $this->generateConfig($baseUrl);
 
-                return new Response(200, ['Content-Type' => 'application/json'], '{"login":"ariadne"}');
-            })
-        ;
-        $templateFactory = $this->getMockBuilder(ProfileTemplateFactory::class)->getMock();
-
-        $config = ProfileConfig::fromArray(['type' => 'github', 'name' => 'GH', 'client' => ['auth' => ['token' => 'ABC', 'type' => 'access_token_header'], 'options' => []], 'templates' => []]);
-
-        $profile = GithubProfile::fromConfig($httpClient, $templateFactory, $config);
+        $profile = GithubProfile::fromConfig($httpClient, $factory, $config);
         $login = $profile->getApiUser();
 
         static::assertSame('ariadne', $login->username);
     }
 
-    public function testCanUseCustomUrl(): void
+    /**
+     * @return iterable<string, array{0: null|string}>
+     */
+    public function getUrls(): iterable
+    {
+        yield 'default' => [null];
+        yield 'custom' => ['https://example.com'];
+    }
+
+    /**
+     * @param array<string, string> $requests
+     */
+    private function mockHttpClient(array $requests): ClientInterface
     {
         $httpClient = $this->getMockBuilder(ClientInterface::class)->getMock();
-        $httpClient
-            ->expects(static::once())
-            ->method('sendRequest')
-            ->willReturnCallback(static function (RequestInterface $request): Response {
-                self::assertSame('GET', $request->getMethod());
-                self::assertSame('https://example.com/api/v3/user', $request->getUri()->__toString());
-                self::assertSame('token ABC', $request->getHeaderLine('Authorization'));
 
-                return new Response(200, ['Content-Type' => 'application/json'], '{"login":"ariadne"}');
-            })
-        ;
-        $templateFactory = $this->getMockBuilder(ProfileTemplateFactory::class)->getMock();
+        foreach ($requests as $url => $response) {
+            $httpClient
+                ->expects(static::once())
+                ->method('sendRequest')
+                ->willReturnCallback(static function (RequestInterface $request) use ($url, $response): Response {
+                    self::assertSame('GET', $request->getMethod());
+                    self::assertSame($url, $request->getUri()->__toString());
+                    self::assertSame('token ABC', $request->getHeaderLine('Authorization'));
 
-        $config = ProfileConfig::fromArray(['type' => 'github', 'name' => 'GH', 'client' => ['url' => 'https://example.com', 'auth' => ['token' => 'ABC', 'type' => 'access_token_header'], 'options' => []], 'templates' => []]);
+                    return new Response(200, ['Content-Type' => 'application/json'], $response);
+                })
+            ;
+        }
 
-        $profile = GithubProfile::fromConfig($httpClient, $templateFactory, $config);
-        $profile->getApiUser();
+        return $httpClient;
+    }
+
+    private function mockTemplateFactory(): ProfileTemplateFactory
+    {
+        return $this->getMockBuilder(ProfileTemplateFactory::class)->getMock();
+    }
+
+    private function generateConfig(?string $url = null): ProfileConfig
+    {
+        $config = ['type' => 'github', 'name' => 'GH', 'client' => ['auth' => ['token' => 'ABC', 'type' => 'access_token_header'], 'options' => []], 'templates' => []];
+        if ($url !== null) {
+            $config['client']['url'] = $url;
+        }
+
+        return ProfileConfig::fromArray($config);
+    }
+
+    private function generateUrl(?string $baseUrl, string $path): string
+    {
+        return sprintf('%1$s%2$s%3$s', $baseUrl ?? 'https://api.github.com', $baseUrl === null ? '' : '/api/v3', $path);
     }
 }
