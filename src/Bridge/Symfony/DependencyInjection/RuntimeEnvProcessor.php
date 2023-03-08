d6d236bbb4bdfa30af71bea1e7f8d1ff32166153
Dalibor Karlović <dalibor.karlovic@sigwin.hr>
2023-03-08 10:09:25 +0100

    feat: XDG based app cache

--- src/Bridge/Symfony/DependencyInjection/RuntimeEnvProcessor.php
+++ src/Bridge/Symfony/DependencyInjection/RuntimeEnvProcessor.php
@@ -1,22 +1,36 @@
 <?php
 
+declare(strict_types=1);
+
+/*
+ * This file is part of the Sigwin Ariadne project.
+ *
+ * (c) sigwin.hr
+ *
+ * This source file is subject to the MIT license that is bundled
+ * with this source code in the file LICENSE.
+ */
+
 namespace Sigwin\Ariadne\Bridge\Symfony\DependencyInjection;
 
 use Sigwin\Ariadne\EnvironmentResolver;
 use Symfony\Component\DependencyInjection\EnvVarProcessorInterface;
 
-class RuntimeEnvProcessor implements EnvVarProcessorInterface
+final class RuntimeEnvProcessor implements EnvVarProcessorInterface
 {
     public function __construct(private readonly EnvironmentResolver $resolver)
-    {}
+    {
+    }
 
     public function getEnv(string $prefix, string $name, \Closure $getEnv): string
     {
-        return $t;
+        return $this->resolver->getCacheDir();
     }
 
     public static function getProvidedTypes(): array
     {
-        return ['cache' => 'string', 'config' => 'string'];
+        return [
+            'xdg_cache_dir' => 'string',
+        ];
     }
 }
--- src/EnvironmentResolver.php
+++ src/EnvironmentResolver.php
@@ -1,8 +1,19 @@
 <?php
 
+declare(strict_types=1);
+
+/*
+ * This file is part of the Sigwin Ariadne project.
+ *
+ * (c) sigwin.hr
+ *
+ * This source file is subject to the MIT license that is bundled
+ * with this source code in the file LICENSE.
+ */
+
 namespace Sigwin\Ariadne;
 
-class EnvironmentResolver
+final class EnvironmentResolver
 {
     public function __construct(private readonly ?string $cache, private readonly ?string $config, private readonly string $home)
     {
@@ -10,11 +21,11 @@ class EnvironmentResolver
 
     public function getCacheDir(): string
     {
-        return $this->cache ?? $this->home .'/.cache/ariadne';
+        return ($this->cache ?? $this->home.'/.cache').'/ariadne';
     }
 
     public function getConfigDir(): string
     {
-        return $this->config ?? $this->home .'/.config/ariadne';
+        return ($this->config ?? $this->home.'/.config').'/ariadne';
     }
 }
