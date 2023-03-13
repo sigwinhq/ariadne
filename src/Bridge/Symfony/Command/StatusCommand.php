0eb564d3bbdbd179157f55312f9020a41adbf5d2
Dalibor Karlović <dalibor.karlovic@sigwin.hr>
2023-03-13 10:59:46 +0100

    chore: rename "test" command to "status" (#69)

--- src/Bridge/Symfony/Command/StatusCommand.php
+++ src/Bridge/Symfony/Command/StatusCommand.php
@@ -21,8 +21,8 @@ use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
 
-#[AsCommand(name: 'ariadne:test', aliases: ['test'])]
-final class TestCommand extends Command
+#[AsCommand(name: 'ariadne:status', aliases: ['status'])]
+final class StatusCommand extends Command
 {
     use CommandTrait;
 
