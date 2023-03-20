e086169495324e31660f0f2b0751cf8c7390d220
Dalibor Karlović <dalibor.karlovic@sigwin.hr>
2023-03-20 17:28:09 +0100

    feat: rename status command to summary (#73)

--- src/Bridge/Symfony/Command/SummaryCommand.php
+++ src/Bridge/Symfony/Command/SummaryCommand.php
@@ -21,8 +21,8 @@ use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
 
-#[AsCommand(name: 'ariadne:status', aliases: ['status'])]
-final class StatusCommand extends Command
+#[AsCommand(name: 'ariadne:summary', aliases: ['summary'])]
+final class SummaryCommand extends Command
 {
     use CommandTrait;
 
