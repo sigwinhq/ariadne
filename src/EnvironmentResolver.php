531b332ae0acd5616f63de67cef3e978bf120645
Dalibor Karlović <dalibor.karlovic@sigwin.hr>
2023-03-21 12:30:24 +0100

    tests: environment resolver

--- src/EnvironmentResolver.php
+++ src/EnvironmentResolver.php
@@ -15,17 +15,17 @@ namespace Sigwin\Ariadne;
 
 final class EnvironmentResolver
 {
-    public function __construct(private readonly ?string $cache, private readonly ?string $config, private readonly string $home)
+    public function __construct(private readonly ?string $cacheHome, private readonly ?string $configHome, private readonly string $home)
     {
     }
 
     public function getCacheDir(): string
     {
-        return ($this->cache ?? $this->home.'/.cache').'/ariadne';
+        return ($this->cacheHome ?? $this->home.'/.cache').'/ariadne';
     }
 
     public function getConfigDir(): string
     {
-        return ($this->config ?? $this->home.'/.config').'/ariadne';
+        return ($this->configHome ?? $this->home.'/.config').'/ariadne';
     }
 }
