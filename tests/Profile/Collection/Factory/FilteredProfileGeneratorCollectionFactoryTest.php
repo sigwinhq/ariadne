0141c4f24844cc42bc3cbcb930464e8b4d8f26df
Dalibor Karlović <dalibor.karlovic@sigwin.hr>
2023-03-21 15:42:27 +0100

    tests: filtered profile generator collection factory

---
+++ tests/Profile/Collection/Factory/FilteredProfileGeneratorCollectionFactoryTest.php
@@ -0,0 +1,45 @@
+<?php
+
+declare(strict_types=1);
+
+/*
+ * This file is part of the Sigwin Ariadne project.
+ *
+ * (c) sigwin.hr
+ *
+ * This source file is subject to the MIT license that is bundled
+ * with this source code in the file LICENSE.
+ */
+
+namespace Sigwin\Ariadne\Test\Profile\Collection\Factory;
+
+use PHPUnit\Framework\TestCase;
+use Sigwin\Ariadne\Model\Config\AriadneConfig;
+use Sigwin\Ariadne\Model\ProfileFilter;
+use Sigwin\Ariadne\Profile\Collection\Factory\FilteredProfileGeneratorCollectionFactory;
+use Sigwin\Ariadne\Profile\Collection\FilteredProfileGeneratorCollection;
+use Sigwin\Ariadne\ProfileFactory;
+
+/**
+ * @internal
+ *
+ * @covers \Sigwin\Ariadne\Profile\Collection\Factory\FilteredProfileGeneratorCollectionFactory
+ *
+ * @uses \Sigwin\Ariadne\Model\Config\AriadneConfig
+ * @uses \Sigwin\Ariadne\Model\ProfileFilter
+ * @uses \Sigwin\Ariadne\Profile\Collection\FilteredProfileGeneratorCollection
+ *
+ * @small
+ */
+final class FilteredProfileGeneratorCollectionFactoryTest extends TestCase
+{
+    public function testWillCreateFilteredProfileGeneratorCollection(): void
+    {
+        $factory = new FilteredProfileGeneratorCollectionFactory($this->createMock(ProfileFactory::class));
+
+        $config = AriadneConfig::fromArray('file:///ariadne.yaml', ['profiles' => []]);
+        $filter = ProfileFilter::create('test', 'type');
+
+        static::assertInstanceOf(FilteredProfileGeneratorCollection::class, $factory->create($config, $filter));
+    }
+}
